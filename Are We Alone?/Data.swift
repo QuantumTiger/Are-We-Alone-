a0ab2334ce9d178a665e4cad0990766acf4675dc
WGonzalez <student@WGonzalez.local>
2016-01-08 10:27:34 -0600

    first commit

---
+++ Are We Alone?/Data.swift
@@ -0,0 +1,13 @@
+//
+//  data.swift
+//  Are We Alone?
+//
+//  Created by WGonzalez on 12/18/15.
+//  Copyright © 2015 Quantum Apple. All rights reserved.
+//
+
+import UIKit
+
+class data: NSObject {
+
+}
---
+++ Are We Alone?/Life.swift
@@ -0,0 +1,21 @@
+//
+//  Life.swift
+//  Are We Alone?
+//
+//  Created by WGonzalez on 12/11/15.
+//  Copyright © 2015 Quantum Apple. All rights reserved.
+//
+
+import UIKit
+
+class Life: UILabel {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
